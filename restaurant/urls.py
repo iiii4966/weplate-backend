936be74af445317778353a303e23031f57832e4c
gapgit <dlarkqrl4966@gmail.com>
2019-09-04 23:13:11 +0900

    RestaurantSearchview 구현 및 NearbyRestaurantListView 구현

---
+++ restaurant/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('main_list', MainRestaurantView.as_view()),
+    path('restaurant', DetailRestaurantView.as_view()),
+    path('restaurant/<int:recommand_id>', NearbyRecommandRestaurantView.as_view()),
+    path('search', SearchRestaurantListView.as_view())
+   ]
